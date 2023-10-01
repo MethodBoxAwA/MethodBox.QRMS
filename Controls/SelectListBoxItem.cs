2cbdf50c150e84896f777dc60a84b9e9144676ec
MethodBox <135963417+MethodBoxAwA@users.noreply.github.com>
2023-10-01 17:26:49 +0800

    QRMS v0.1b
    1. 初步开始开发右侧项目模板菜单；
    2. 项目模板控件开发。

---
+++ Controls/SelectListBoxItem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MethodBox.QRMS.Controls
+{
+    public class SelectListBoxItem : ListBoxItem
+    {
+        static SelectListBoxItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SelectListBoxItem), new FrameworkPropertyMetadata(typeof(SelectListBoxItem)));
+        }
+
+        public string Context
+        {
+            get { return (string)GetValue(ContextProperty); }
+            set { SetValue(ContextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Context.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContextProperty =
+            DependencyProperty.Register("Context", typeof(string), typeof(SelectListBoxItem));
+
+        public ImageSource ImageSource
+        {
+            get { return (ImageSource)GetValue(ImageSourceProperty); }
+            set { SetValue(ImageSourceProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ImageSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageSourceProperty =
+            DependencyProperty.Register("ImageSource", typeof(ImageSource), typeof(SelectListBoxItem));
+
+
+    }
+
+}
